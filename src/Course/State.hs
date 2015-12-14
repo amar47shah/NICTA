5209886543848f2fbce46bfb0c001095594d8e50
Amar Shah <amar47shah@gmail.com>
2015-12-13 23:41:09 -0500

    Continue Course.State

--- src/Course/State.hs
+++ src/Course/State.hs
@@ -96,7 +96,7 @@ exec ::
   -> s
   -> s
 exec =
-  error "todo: Course.State#exec"
+  (snd .) . runState
 
 -- | Run the `State` seeded with `s` and retrieve the resulting value.
 --
@@ -106,7 +106,7 @@ eval ::
   -> s
   -> a
 eval =
-  error "todo: Course.State#eval"
+  (fst .) . runState
 
 -- | A `State` where the state also distributes into the produced value.
 --
@@ -115,7 +115,7 @@ eval =
 get ::
   State s s
 get =
-  error "todo: Course.State#get"
+  State $ \s -> (s, s)
 
 -- | A `State` where the resulting state is seeded with the given value.
 --
@@ -125,7 +125,7 @@ put ::
   s
   -> State s ()
 put =
-  error "todo: Course.State#put"
+  State . const . (,) ()
 
 -- | Find the first element in a `List` that satisfies a given predicate.
 -- It is possible that no element is found, hence an `Optional` result.
@@ -146,8 +146,10 @@ findM ::
   (a -> f Bool)
   -> List a
   -> f (Optional a)
-findM =
-  error "todo: Course.State#findM"
+findM p (x :. xs) = p x >>= dependsM
+  where dependsM True  = return $ Full x
+        dependsM False = findM p xs
+findM _ _ = return Empty
 
 -- | Find the first element in a `List` that repeats.
 -- It is possible that no element repeats, hence an `Optional` result.
@@ -160,8 +162,16 @@ firstRepeat ::
   Ord a =>
   List a
   -> Optional a
-firstRepeat =
-  error "todo: Course.State#firstRepeat"
+firstRepeat xs = eval (findM p xs) S.empty
+    where p x = State $ \s -> (S.member x s, S.insert x s)
+--
+-- Solution without `State` and `findM`:
+--
+-- firstRepeat = go S.empty
+--   where go memo (x :. xs)
+--          | S.member x memo = Full x
+--          | otherwise       = go (S.insert x memo) xs
+--         go _ _ = Empty
 
 -- | Remove all duplicate elements in a `List`.
 -- /Tip:/ Use `filtering` and `State` with a @Data.Set#Set@.
@@ -173,8 +183,8 @@ distinct ::
   Ord a =>
   List a
   -> List a
-distinct =
-  error "todo: Course.State#distinct"
+distinct xs = eval (filtering p xs) S.empty
+  where p x = State $ \s -> (S.notMember x s, S.insert x s)
 
 -- | A happy number is a positive integer, where the sum of the square of its digits eventually reaches 1 after repetition.
 -- In contrast, a sad number (not a happy number) is where the sum of the square of its digits never reaches 1
