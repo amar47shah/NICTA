19021bb57d2c904677a32d20210a20c3db9d9e62
Amar Shah <amar47shah@gmail.com>
2016-01-26 11:36:30 -0500

    Implement `fastAnagrams` using Data.Map

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -6,16 +6,24 @@ module Course.FastAnagrams where
 import Course.Core
 import Course.List
 import Course.Functor
-import qualified Data.Set as S
+import qualified Data.Map as M
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
+counts :: Chars -> M.Map Char Int
+counts = foldRight (\c -> M.insertWith (+) c 1) M.empty
+
+match :: Chars -> Chars -> Bool
+match = (==) `on` counts
+
 fastAnagrams ::
   Chars
   -> Filename
   -> IO (List Chars)
-fastAnagrams =
-  error "todo: Course.FastAnagrams#fastAnagrams"
+fastAnagrams cs filename =
+  filter (match cs) . lines <$> readFile filename
+
+-- Ignoring this:
 
 newtype NoCaseString =
   NoCaseString {
